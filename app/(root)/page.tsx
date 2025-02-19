023c60b8165a6da5c1b0fa0177e1f96a9703df87
Yunus Habeeb <habeebdh1@gmail.com>
2025-02-19 05:52:49 +0100

    Add root layout, page, and update global styles with background pattern

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -3,7 +3,6 @@ import { Button } from '@/components/ui/button';
 export default function Home() {
   return (
     <div>
-      <h1>BookWise</h1>
       <Button>Click me</Button>
     </div>
   );
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -104,6 +104,9 @@ export default {
         md: 'calc(var(--radius) - 2px)',
         sm: 'calc(var(--radius) - 4px)',
       },
+      backgroundImage: {
+        pattern: "url('/images/pattern.webp')",
+      },
     },
   },
   plugins: [require('tailwindcss-animate')],
